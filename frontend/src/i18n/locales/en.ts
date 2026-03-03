8f0b567e40b4208b7860596f531cbf78f2322e9a
Matteo Parenti <matteoparenti02@gmail.com>
2026-03-03 15:39:22 +0100

    Add button localisation

--- frontend/src/i18n/locales/en.ts
+++ frontend/src/i18n/locales/en.ts
@@ -522,6 +522,7 @@ export default {
     manageTags: 'Manage Tags',
     showFailedPrints: 'Show failed prints',
     hideFailedPrints: 'Hide failed prints',
+    hideDuplicates: 'Hide Duplicates',
     printTime: 'Print Time',
     filamentUsed: 'Filament Used',
     cost: 'Cost',
